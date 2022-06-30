0aa97cad8e7ef9fdd365d16eb829659fc44a81ca
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2022-06-29 20:48:33 -0400

    created file

---
+++ test/3.rock_paper_scissors-logic.test.js
@@ -0,0 +1,36 @@
+import {RockPaperScissors} from './rock_paper_scissors.js';
+const { RockPaperScissors } = require(`../resources/scripts/rock_paper_scissors.js`);
+const mathRandomSpy = jest.spyOn(Math, 'random');
+describe(`RockPaperScissors class`, function () {
+  describe(`determineWinner()`, function () {
+    test(`win cases`, function () {
+      const game = new RockPaperScissors();
+      expect(game.determineWinner(`rock`, `scissors`)).toBe(`win`);
+      // Complete the test
+    });
+
+    test(`tie cases`, function () {
+      // Write your test here
+    });
+
+    test(`lost cases`, function () {
+      // Write your test here
+    });
+  });
+
+  describe('generateCPURespones()', function () {
+    it('Math.Random = 0.1 -> Rock', function(){
+      mathRandomSpy.mockImplementation(() => 0.1);
+      const game = new RockPaperScissors();
+      expect(game.generateCPUResponse()).toBe('rock');
+    });
+    
+    it('Math.Random = 0.5 -> Paper', function() {
+      //Wite your test here
+    });
+
+    it('Math.Random = 0.9 -> Paper', function() {
+      //Write your test here
+    });
+  });
+});
